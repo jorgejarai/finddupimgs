8dba6a47db295109a5abf273a7e77b9451741f6b
Jorge Jara <jorgejarainostroza@gmail.com>
2023-07-19 16:50:11 -0400

    Make text unselectable

--- src/App.svelte
+++ src/App.svelte
@@ -29,9 +29,9 @@
 
 <div class="flex h-screen flex-col overflow-hidden">
   <header class="flex w-full items-center gap-4 bg-slate-200 p-4">
-    <span class="text-xl">finddupimgs</span>
+    <span class="cursor-default select-none text-xl">finddupimgs</span>
     <div class="flex flex-grow items-center gap-2">
-      <span>Searching in:</span>
+      <span class="cursor-default select-none">Searching in:</span>
       <input
         type="text"
         class="flex-grow rounded-md border border-gray-400 p-1"
@@ -56,14 +56,17 @@
   {#if selectedImages.length > 0}
     <div class="flex items-center justify-between bg-red-300 p-3">
       {#if selectedImages.length == 1}
-        <span class="text-red-900">1 image selected</span>
+        <span class="cursor-default select-none text-red-900"
+          >1 image selected</span
+        >
       {:else if selectedImages.length > 1}
-        <span class="text-red-900">{selectedImages.length} images selected</span
+        <span class="cursor-default select-none text-red-900"
+          >{selectedImages.length} images selected</span
         >
       {/if}
 
       <button
-        class="rounded bg-red-400 px-4 py-1 text-sm font-bold transition-colors hover:bg-red-500"
+        class="cursor-default select-none rounded bg-red-400 px-4 py-1 text-sm font-bold transition-colors hover:bg-red-500"
       >
         Delete
       </button>
@@ -73,16 +76,20 @@
   <footer class="w-screen bg-slate-100 px-2 py-1 text-sm">
     {#if selectedPath != ''}
       {#if output.length == 1}
-        <span> 1 group of duplicates found </span>
+        <span class="cursor-default select-none">
+          1 group of duplicates found
+        </span>
       {:else if output.length > 1}
-        <span>
+        <span class="cursor-default select-none">
           {output.length} groups of duplicates found
         </span>
       {:else}
-        <span>No duplicates found</span>
+        <span class="cursor-default select-none">No duplicates found</span>
       {/if}
     {:else}
-      <span>Choose a directory to search</span>
+      <span class="cursor-default select-none"
+        >Choose a directory to search</span
+      >
     {/if}
   </footer>
 </div>
--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -12,5 +12,9 @@
   class="flex cursor-pointer flex-col items-center p-2 transition-colors hover:bg-gray-100"
 >
   <img src={imgUrl} class="mb-1 h-36 w-36 object-cover" alt={imgUrl} />
-  <span title={filename} class="w-36 break-all text-center">{filename}</span>
+  <span
+    title={filename}
+    class="w-36 cursor-default select-none break-all text-center"
+    >{filename}</span
+  >
 </div>
